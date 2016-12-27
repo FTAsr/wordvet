54ea7326ba7a2ea1e2377e94e7530b9eef5a1b98
FA23 <fatemehtorabiasr@yahoo.com>
2016-12-27 18:11:49 -0500

    first version of trainer/tester added

---
+++ trainer-version01.sh
@@ -0,0 +1,2 @@
+echo "Training on wiki-en.txt with parameters: -min_count 5 -outputw vectorsW.txt -outputc vectorsC.txt -outputwc vectorsB.txt -cbow 0 -size 300 -window 10 -negative 5"
+time ./word2vec -train wiki-en.txt -min_count 5 -outputw vectorsW.txt -outputc vectorsC.txt -outputwc vectorsB.txt -cbow 0 -size 300 -window 10 -negative 5 
