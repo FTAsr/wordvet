dd226c0a9b3cf75bc77933e0852295688af72690
Rob Zinkov <rob@zinkov.com>
2016-12-28 16:25:09 -0500

    Commit initial word2vec

---
+++ word2vec/demo-word.sh
@@ -0,0 +1,8 @@
+#comment
+make
+#if [ ! -e text8 ]; then
+#  wget http://mattmahoney.net/dc/text8.zip -O text8.gz
+#  gzip -d text8.gz -f
+#fi
+time ./word2vec -train ElmanCorpus_distributionuniform_size10000.txt -min_count 1 -outputw vectorsW.txt -outputc vectorsC.txt -outputwc vectorsB.txt -cbow 0 -size 14 -window 2 -negative 1 -hs 1 -sample 0 -threads 1 -binary 0 -iter 1
+./distance vectors.bin
