4d2c8e3359a191b18455aaaa54e92423f50e335d
FA23 <fatemehtorabiasr@yahoo.com>
2017-01-09 16:19:52 -0500

    changed demo-phrase

--- word2vec/demo-phrases.sh
+++ word2vec/demo-phrases.sh
@@ -1,11 +1,8 @@
 make
-if [ ! -e news.2012.en.shuffled ]; then
-  wget http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2012.en.shuffled.gz
-  gzip -d news.2012.en.shuffled.gz -f
-fi
-sed -e "s/’/'/g" -e "s/′/'/g" -e "s/''/ /g" < news.2012.en.shuffled | tr -c "A-Za-z'_ \n" " " > news.2012.en.shuffled-norm0
-time ./word2phrase -train news.2012.en.shuffled-norm0 -output news.2012.en.shuffled-norm0-phrase0 -threshold 200 -debug 2
-time ./word2phrase -train news.2012.en.shuffled-norm0-phrase0 -output news.2012.en.shuffled-norm0-phrase1 -threshold 100 -debug 2
-tr A-Z a-z < news.2012.en.shuffled-norm0-phrase1 > news.2012.en.shuffled-norm1-phrase1
-time ./word2vec -train news.2012.en.shuffled-norm1-phrase1 -output vectors-phrase.bin -cbow 1 -size 200 -window 10 -negative 25 -hs 0 -sample 1e-5 -threads 20 -binary 1 -iter 15
-./distance vectors-phrase.bin
+
+sed -e "s/’/'/g" -e "s/′/'/g" -e "s/''/ /g" < wiki.txt | tr -c "A-Za-z'_ \n" " " > wiki.shuffled-norm0
+time ./word2phrase -train wiki.shuffled-norm0 -output wiki.shuffled-norm0-phrase0 -threshold 200 -debug 2
+time ./word2phrase -train wiki.shuffled-norm0-phrase0 -output wiki.shuffled-norm0-phrase1 -threshold 100 -debug 2
+tr A-Z a-z < wiki.shuffled-norm0-phrase1 > wiki.shuffled-norm1-phrase1
+time ./word2vec -train wiki.shuffled-norm1-phrase1 -min_count 1 -outputw vectorsW.txt -outputc vectorsC.txt -outputwc vectorsB.txt -cbow 0 -size 100 -window 6 -negative 1 -hs 1 -sample 0 -threads 1 -binary 0 -iter 1
+./distance vectors.bin
