4d5e5126df7c31714ac60e92a614effcda52d464
FA23 <fatemehtorabiasr@yahoo.com>
2017-04-04 19:39:15 -0400

    working with glove

--- trainer-version01.sh
+++ trainer-version01.sh
@@ -1,2 +1,3 @@
+echo "Example training for modified word2vec (with both word and context vectors dump)"
 echo "Training on wiki-en.txt with parameters: -min_count 5 -outputw vectorsW.txt -outputc vectorsC.txt -outputwc vectorsB.txt -cbow 0 -size 300 -window 10 -negative 5"
 time ./word2vec wiki-en.txt with parameters: -min_count 5 -outputw vectorsW.txt -outputc vectorsC.txt -outputwc vectorsB.txt -cbow 0 -size 300 -window 10 -negative 5 
