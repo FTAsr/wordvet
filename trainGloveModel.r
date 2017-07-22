fde50fce0c33bba3e015bc30fc35ef99cea27e3d
Rob Zinkov <rob@zinkov.com>
2017-07-22 05:29:10 -0400

    Make glove easier to instrument

--- trainGloveModel.r
+++ trainGloveModel.r
@@ -1,14 +1,15 @@
-
-##pre-requisites:
-#library(devtools)
-#load_all("text2vec")
-#install("text2vec")
-#build("text2vec")
-
-
 print("started running trainGloveModel.r")
 
 library("text2vec")
+
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) != 3) {
+  cat("trainGloveModel.R <size> <window> <iters>\n")
+} else {
+
+vectorSize   <- as.numeric(args[1])
+window       <- as.numeric(args[2])
+iters        <- as.numeric(args[3])
 
 text8_file = "/data/wiki.shuffled-norm1-phrase1"
 
@@ -19,11 +20,11 @@ tokens <- space_tokenizer(wiki)
 it = itoken(tokens, progressbar = FALSE)
 
 vocab <- create_vocabulary(it)
-vocab <- prune_vocabulary(vocab, doc_proportion_max = 0.3, 
+vocab <- prune_vocabulary(vocab, doc_proportion_max = 0.3,
                           max_number_of_terms = 30000L, term_count_min = 5L)
 # Use our filtered vocabulary
-vectorizer <- vocab_vectorizer(vocab, skip_grams_window=10) 
-                               
+vectorizer <- vocab_vectorizer(vocab, skip_grams_window=window)
+
 tcm <- create_tcm(it, vectorizer)
 
 ind <- tcm@x >= 1
@@ -31,8 +32,8 @@ tcm@x <- tcm@x[ind]
 tcm@i <- tcm@i[ind]
 tcm@j <- tcm@j[ind]
 
-glove = GlobalVectors$new(word_vectors_size = 300, vocabulary = vocab, x_max = 10)
-glove$fit(tcm, n_iter = 10)
+glove = GlobalVectors$new(word_vectors_size = vectorSize, vocabulary = vocab, x_max = 10)
+glove$fit(tcm, n_iter = iters)
 
 dd <- glove$dump_model()
 wordVec = dd$w_i
@@ -40,20 +41,24 @@ contextVec = dd$w_j
 listVocab = vocab$vocab[,1]
 
 W = cbind(listVocab, wordVec)
-fileConn<-file("/data/model_200_10/vectorsW.txt")
+modelDir <- paste("/data/model_", size, "_", window, sep="")
+dir.create(modelDir, showWarnings=False)
+
+vectorsW <- paste(modelDir, "/vectorsW.txt", sep="")
+fileConn <- file(vectorsW)
 writeLines(paste(nrow(wordVec), ncol(wordVec)),fileConn )
 close(fileConn)
-write.table(W, file = "/data/model_200_10/vectorsW.txt", append = TRUE, quote = FALSE, sep = " ",
+write.table(W, file = vectorsW, append = TRUE, quote = FALSE, sep = " ",
             eol = "\n", dec = ".", row.names = FALSE,
             col.names = FALSE, qmethod = c("escape", "double"),
             fileEncoding = "")
 
-C = cbind(listVocab, contextVec)           
-fileConn<-file("/data/model_200_10/vectorsC.txt")
+vectorsC <- paste(modelDir, "/vectorsC.txt", sep="")
+C = cbind(listVocab, contextVec)
+fileConn < -file(vectorsC)
 writeLines(paste(nrow(contextVec), ncol(contextVec)),fileConn )
 close(fileConn)
-write.table(C, file = "/data/model_200_10/vectorsC.txt", append = TRUE, quote = FALSE, sep = " ",
+write.table(C, file = vectorsC, append = TRUE, quote = FALSE, sep = " ",
             eol = "\n", dec = ".", row.names = FALSE,
             col.names = FALSE, qmethod = c("escape", "double"),
             fileEncoding = "")
-
