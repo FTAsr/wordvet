6489dc7272e82ce439b5c101854e36cb66b1c83d
Rob Zinkov <rob@zinkov.com>
2018-06-01 12:35:45 +0100

    Cleanup

--- trainer-version01.sh
+++ trainer-version01.sh
@@ -1,3 +1,11 @@
-echo "Example training for modified word2vec (with both word and context vectors dump)"
-echo "Training on wiki-en.txt with parameters: -min_count 5 -outputw vectorsW.txt -outputc vectorsC.txt -outputwc vectorsB.txt -cbow 0 -size 300 -window 10 -negative 5"
-time ./word2vec wiki-en.txt with parameters: -min_count 5 -outputw vectorsW.txt -outputc vectorsC.txt -outputwc vectorsB.txt -cbow 0 -size 300 -window 10 -negative 5 
+#!/bin/bash
+set -eu
+set -o pipefail
+if [ "$#" -ne 3 ] ; then
+  echo "word2vec_wrap <size> <window> <iters>"
+  exit 0
+fi
+echo "Training for modified word2vec (with both word and context vectors dump)"
+mkdir -p data/word2vec_$1_$2
+./word2vec/word2vec -train wiki.shuffled-norm1-phrase1 -min_count 1 -outputw data/word2vec_$1_$2/vectorsW.txt -outputc data/word2vec_$1_$2/vectorsC.txt -outputwc data/word2vec_$1_$2/vectorsB.txt -cbow 0 -size $1 -window $2 -negative 5 -threads 20 -iter $3
+
